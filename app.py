8d5cb97b7866b88dd19e912798a09d502e75507d
Joffrey Kern <joffrey.kern@asos.com>
2019-01-25 15:43:09 +0000

    Turn json list into models

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
 import flask
-from flask import request, jsonify, abort
+import jsonpickle
+from flask import request
+from flask.json import JSONEncoder
+
+from models.book import Book
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        return JSONEncoder.default(self, obj)
+
 
 app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+app.json_encoder = CustomJSONEncoder
+
+books = [
+    Book(0, 'A Fire Upon the Deep', 'Vernor Vinge', '1992'),
+    Book(1, 'The Ones Who Walk Away From Omelas', 'Ursula K. Le Guin', '1973'),
+    Book(2, 'Dhalgren', 'Samuel R. Delany', '1975')
+]
+
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Hello World</h1>"
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
 
 @app.route('/api/v1/books', methods=['GET'])
 def get_books():
     if 'id' in request.args:
         id = int(request.args['id'])
     else:
-        return jsonify(books)
+        return jsonpickle.encode(books)
 
-    book = (list(filter(lambda x: x['id'] == id, books)))
-    return jsonify(book)
+    book = (list(filter(lambda x: x.id == id, books)))
+    return jsonpickle.encode(book)
+
+
+app.run()
---
+++ models/book.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, id, title, author, published):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.published = published
